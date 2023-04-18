14aa099351ad90e391a8a0a7421e766b0f5d53cb
Wilson Efrain Vargas Marin <wilsonvargas_6@outlook.com>
2023-04-18 11:44:43 -0500

    adding last configuration to docker file

--- Client/src/HttpCommon.js
+++ Client/src/HttpCommon.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: process.env.REACT_APP_BACKEND_URL_BASE + '/api',
     headers: {
         'Content-type': 'application/json',
     },
