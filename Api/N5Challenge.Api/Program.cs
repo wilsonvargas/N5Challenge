1f2c7a7df81f26bfbdc03683aabdf745de44fba5
Wilson Efrain Vargas Marin <wilsonvargas_6@outlook.com>
2023-04-18 12:16:48 -0500

    Adding migrations to startup project

--- Api/N5Challenge.Api/Program.cs
+++ Api/N5Challenge.Api/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using N5Challenge.Domain;
 
 namespace N5Challenge
 {
@@ -13,7 +16,13 @@ namespace N5Challenge
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<N5ChallengeDbContext>();
+                db.Database.Migrate();
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
