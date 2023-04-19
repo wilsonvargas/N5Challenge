df7171f14a80e02beeb42e6c4573d9eedadeb004
Wilson Efrain Vargas Marin <wilsonvargas_6@outlook.com>
2023-04-19 18:11:55 -0500

    Adding unit test

---
+++ Api/N5Challenge.Api.Tests/Mocks/MediatorMockData.cs
@@ -0,0 +1,33 @@
+﻿using N5Challenge.Domain.Entities;
+using N5Challenge.Mediator.Commands;
+using N5Challenge.Mediator.Queries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace N5Challenge.Api.Tests.Mocks
+{
+    public class MediatorMockData
+    {
+        public static Permission GetPermissionData()
+        {
+            return new Permission
+            {
+                Id = 1,
+                EmployeeName = "Saul",
+                EmployeeLastName = "Goodman",
+                PermissionDate = DateTime.Now,
+                PermissionTypeId = 1
+            };
+        }
+
+        public static PermissionType GetPermissionTypeData()
+        {
+            return new PermissionType
+            {
+                Id = 1,
+                Description = "Read"
+            };
+        }
+    }
+}
