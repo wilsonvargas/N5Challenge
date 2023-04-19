633c5b1bdd952c927588b6ddea4006ea35a0d9f4
Wilson Efrain Vargas Marin <wilsonvargas_6@outlook.com>
2023-04-19 10:33:32 -0500

    Minor fixes into initial migration

--- Api/N5Challenge.Domain/N5ChallengeDbContext.cs
+++ Api/N5Challenge.Domain/N5ChallengeDbContext.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using N5Challenge.Domain.Configuration;
 using N5Challenge.Domain.Entities;
 
 namespace N5Challenge.Domain
@@ -9,11 +8,5 @@ namespace N5Challenge.Domain
         public N5ChallengeDbContext(DbContextOptions<N5ChallengeDbContext> options) : base(options) { }
 
         public DbSet<Permission> Permissions { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.ApplyConfiguration(new PermissionTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new PermissionConfiguration());
-        }
     }
 }
